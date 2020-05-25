7054ada48b5d946255d55c15a32bd6f01beb5724
Riccsard <58710642+Riccsard@users.noreply.github.com>
2020-05-25 19:57:11 +0200

    rancfelvarras

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function closeNav() {
   document.getElementById("hamb").setAttribute('onclick', 'openNav()');
   document.getElementById("hamb").innerHTML = "&#9776;";
   document.getElementById("social").style.right = "-100%";
+  window.location.href = 'index.html';
 }
 
 
@@ -26,7 +27,15 @@ function closeProj() {
   document.getElementById("hambtwo").innerHTML = "&#9776;";
 }
 
+function social(ido) {
+  if (ido==0) {
+    document.getElementById("background").style.backgroundImage= "url('/bgold.jpg')";
+    document.getElementById("circlehead").style.backgroundImage= "url('/circlepic.png')";
+    document.getElementById("chromie").setAttribute('onclick', 'social(1)');
+  } if (ido==1) {
+    document.getElementById("background").style.backgroundImage= "url('/bg.jpg')";
+    document.getElementById("circlehead").style.backgroundImage= "url('/portrait.png')";
+    document.getElementById("chromie").setAttribute('onclick', 'social(0)');
+  }
 
-function social() {
-  document.getElementById("social").style.right = "5%";
 }
